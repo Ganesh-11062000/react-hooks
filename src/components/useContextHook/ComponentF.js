76ced11396d99fac0dc4634fd74af4204fd59bd9
Ganesh Moota <ganeshnm2000@gmail.com>
2021-04-28 16:40:25 +0530

    understanding usecase of useContext in place of context.Consumer

--- src/components/useContextHook/ComponentF.js
+++ src/components/useContextHook/ComponentF.js
@@ -1,25 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { userContext, channelContext } from "../../App";
 
 function ComponentF() {
+  const user = useContext(userContext);
+  const channel = useContext(channelContext);
+
   return (
     <div className="text-center">
       component F
-      <channelContext.Consumer>
-        {(channel) => {
-          return (
-            <userContext.Consumer>
-              {(user) => {
-                return (
-                  <h1>
-                    {user} is learning from {channel} channel
-                  </h1>
-                );
-              }}
-            </userContext.Consumer>
-          );
-        }}
-      </channelContext.Consumer>
+      <h1>
+        {user} is learning from {channel} channel
+      </h1>
     </div>
   );
 }
