e4822806c79e972a92f0c391229a56c9ece18bc3
Ganesh Moota <ganeshnm2000@gmail.com>
2021-04-28 17:39:10 +0530

    standard patterns of using useReducer similar to REDUX

--- src/components/useReducer/CounterTwo.js
+++ src/components/useReducer/CounterTwo.js
@@ -1,14 +1,36 @@
 import React, { useReducer } from "react";
 
-const initialState = { firstCount: 0 };
+const initialState = {
+  firstCount: 0,
+  secondCount: 0,
+};
+
 const reducer = (currentState, action) => {
   switch (action.type) {
     case "INCREMENT":
-      return { firstCount: currentState.firstCount + 1 };
+      return {
+        ...currentState,
+        firstCount: currentState.firstCount + action.value,
+      };
     case "DECREMENT":
-      return { firstCount: currentState.firstCount - 1 };
+      return {
+        ...currentState,
+        firstCount: currentState.firstCount - action.value,
+      };
     case "RESET":
-      return { firstCount: 0 };
+      return { ...currentState, firstCount: 0 };
+    case "INCREMENT2":
+      return {
+        ...currentState,
+        secondCount: currentState.secondCount + action.value,
+      };
+    case "DECREMENT2":
+      return {
+        ...currentState,
+        secondCount: currentState.secondCount - action.value,
+      };
+    case "RESET2":
+      return { ...currentState, secondCount: 0 };
     default:
       return currentState;
   }
@@ -23,19 +45,65 @@ function CounterTwo() {
       <div className="btn-group">
         <button
           className="btn btn-group-item btn-secondary"
-          onClick={() => dispatch({ type: "INCREMENT" })}
+          onClick={() => dispatch({ type: "INCREMENT", value: 1 })}
         >
           Increment
         </button>
         <button
           className="btn btn-group-item btn-secondary"
-          onClick={() => dispatch({ type: "DECREMENT" })}
+          onClick={() => dispatch({ type: "DECREMENT", value: 1 })}
+        >
+          Decrement
+        </button>
+        <button
+          className="btn btn-group-item btn-secondary"
+          onClick={() => dispatch({ type: "INCREMENT", value: 5 })}
+        >
+          Increment 5
+        </button>
+        <button
+          className="btn btn-group-item btn-secondary"
+          onClick={() => dispatch({ type: "DECREMENT", value: 5 })}
         >
           Decrement
         </button>
         <button
           className="btn btn-group-item btn-secondary"
           onClick={() => dispatch({ type: "RESET" })}
+        >
+          Reset
+        </button>
+      </div>
+
+      <h1>Counter: {count.secondCount}</h1>
+      <div className="btn-group">
+        <button
+          className="btn btn-group-item btn-primary"
+          onClick={() => dispatch({ type: "INCREMENT2", value: 1 })}
+        >
+          Increment
+        </button>
+        <button
+          className="btn btn-group-item btn-primary"
+          onClick={() => dispatch({ type: "DECREMENT2", value: 1 })}
+        >
+          Decrement
+        </button>
+        <button
+          className="btn btn-group-item btn-primary"
+          onClick={() => dispatch({ type: "INCREMENT2", value: 5 })}
+        >
+          Increment 5
+        </button>
+        <button
+          className="btn btn-group-item btn-primary"
+          onClick={() => dispatch({ type: "DECREMENT2", value: 5 })}
+        >
+          Decrement
+        </button>
+        <button
+          className="btn btn-group-item btn-primary"
+          onClick={() => dispatch({ type: "RESET2" })}
         >
           Reset
         </button>
