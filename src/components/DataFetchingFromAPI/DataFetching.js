e4c8dafbbb36a0a52633327c5add934199ce7bb6
Ganesh Moota <ganeshnm2000@gmail.com>
2021-04-28 08:57:37 +0530

    fetching a single post using useEffect dependencyList

--- src/components/DataFetchingFromAPI/DataFetching.js
+++ src/components/DataFetchingFromAPI/DataFetching.js
@@ -2,27 +2,35 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 
 function DataFetching() {
-  const [posts, setPosts] = useState([]);
+  const [post, setPost] = useState([]);
+  const [id, setId] = useState(1);
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1);
 
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then((res) => {
         console.log(res);
-        setPosts(res.data);
+        setPost(res.data);
       })
       .catch((err) => {
         console.log(err.message);
       });
-  },[]);
+  }, [idFromButtonClick]);
+
+  const handleClick = () => {
+    if (id !== "") {
+      setIdFromButtonClick(id);
+    }
+  };
 
   return (
-    <div>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
+    <div className="text-center mt-5">
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <button type="button" onClick={handleClick}>
+        Fetch Post
+      </button>
+      <p>{post.title}</p>
     </div>
   );
 }
